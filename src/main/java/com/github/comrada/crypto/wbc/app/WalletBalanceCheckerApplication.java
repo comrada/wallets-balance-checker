db99ee132ba148e807de62b2c5a0031b42a828d2
Pavel Lobyrin <pavel@lobyrin.ch>
2022-06-19 11:15:12 +0200

    added Spring app

---
+++ src/main/java/com/github/comrada/crypto/wbc/app/WalletBalanceCheckerApplication.java
@@ -0,0 +1,12 @@
+package com.github.comrada.crypto.wbc.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletBalanceCheckerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WalletBalanceCheckerApplication.class, args);
+  }
+}
