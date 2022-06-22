b326561d9c4ff0406a9f5958df8019ca445ac582
Pavel Lobyrin <pavel@lobyrin.ch>
2022-06-22 18:42:28 +0200

    added storage poller config

---
+++ src/main/java/com/github/comrada/crypto/wbc/app/config/StoragePollerConfig.java
@@ -0,0 +1,31 @@
+package com.github.comrada.crypto.wbc.app.config;
+
+import com.github.comrada.crypto.wbc.checker.BalanceUpdater;
+import com.github.comrada.crypto.wbc.checker.NetworksManager;
+import com.github.comrada.crypto.wbc.checker.WalletStorage;
+import com.github.comrada.crypto.wbc.checker.scheduler.DelayGenerator;
+import com.github.comrada.crypto.wbc.checker.scheduler.LimitedFixedDelayGenerator;
+import com.github.comrada.crypto.wbc.checker.scheduler.StoragePoller;
+import java.time.Duration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StoragePollerConfig {
+
+  @Bean
+  StoragePoller storagePoller(WalletStorage walletStorage, BalanceUpdater balanceUpdater,
+      NetworksManager networksManager, DelayGenerator delayGenerator) {
+    return new StoragePoller(walletStorage, delayGenerator, networksManager.assets(), balanceUpdater);
+  }
+
+  @Bean
+  DelayGenerator delayGenerator() {
+    return new LimitedFixedDelayGenerator(Duration.ofSeconds(5), 3);
+  }
+
+  @Bean
+  BalanceUpdater walletHandler(NetworksManager networksManager, WalletStorage walletStorage) {
+    return new BalanceUpdater(networksManager, walletStorage);
+  }
+}
